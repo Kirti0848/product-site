112e34eaf6ff2d5b0324471b84b4d6dfdd2472b9
Kirti0848 <kirti0848@gmail.com>
2026-03-31 13:12:25 +0530

    chore: update build output directory for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8081'
